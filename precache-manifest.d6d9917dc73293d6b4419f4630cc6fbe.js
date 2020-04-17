055124b98c4404c04ef5e94f8117ec0d6fbe98d6
Ben Carlson <bjcarlson42@outlook.com>
2020-04-16 23:02:51 -0400

    Updates

---
+++ precache-manifest.d6d9917dc73293d6b4419f4630cc6fbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f04e315d1b0c3a6cb883d490a1d3aa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e067a714c931a4f22067",
+    "url": "/static/css/main.6f9d6ffa.chunk.css"
+  },
+  {
+    "revision": "ff14225236cfd07d4b00",
+    "url": "/static/js/2.1ffeca1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ffeca1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e067a714c931a4f22067",
+    "url": "/static/js/main.ea7b6e3e.chunk.js"
+  },
+  {
+    "revision": "6e3af85db26b55e2f0db",
+    "url": "/static/js/runtime-main.1a1a023b.js"
+  },
+  {
+    "revision": "cec525d9d8970a5b06a00668685607c8",
+    "url": "/static/media/contact.cec525d9.svg"
+  },
+  {
+    "revision": "36433959e8388349aad6dc78d852dd04",
+    "url": "/static/media/experience-mirror-man.36433959.svg"
+  },
+  {
+    "revision": "241267804271237c244be43b944b465a",
+    "url": "/static/media/greet-man.24126780.svg"
+  },
+  {
+    "revision": "7b92cd8f07d7c9967c382673682355e4",
+    "url": "/static/media/projects.7b92cd8f.svg"
+  }
+]);
